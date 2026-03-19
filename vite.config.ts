f7569d4955d10c887d0a35c9c9cc0b61f9eabf92
martimayoral <76410086+martimayoral@users.noreply.github.com>
2026-03-19 18:20:40 +0100

    refactor: update ESLint configuration and add Biome setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -6,4 +6,4 @@ export default defineConfig({
     port: 8080,
     open: true,
   },
-});
+})
